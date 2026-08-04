be854fed8ff1ffe72a09b916f7a91216d01a52b7
Tle <title1337@gmail.com>
2026-08-04 20:14:38 +0700

    Feat: bookroutes

--- server/src/routes/book.routes.js
+++ server/src/routes/book.routes.js
@@ -1,2 +1,11 @@
+import { Router } from 'express';
+import authenticate from '../middlewares/authenticate.middleware.js';
+import { getAllBooks } from '../controllers/book.controller.js';
 
+const bookRouter = Router();
 
+bookRouter.use(authenticate);
+
+bookRouter.get('/', getAllBooks);
+
+export default bookRouter;
